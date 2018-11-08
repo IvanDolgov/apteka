2fd15f10dc2aebe429008f14b1f808cac1be7560
Ivan D <wanttobox@gmail.com>
2018-11-08 17:35:06 +0300

    Update main.py
    change structure json file

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ def hello(message):
 def apteka(message):
     if message.chat.id in config.adminid:
         keyboard = types.ReplyKeyboardMarkup(True, False)
-        button_1 = types.KeyboardButton(text="Проверить лекарство")
+        button_1 = types.KeyboardButton(text="Добавить лекарство")
         button_2 = types.KeyboardButton(text="Удалить лекарство")
-        button_3 = types.KeyboardButton(text="Добавить лекарство")
-        button_4 = types.KeyboardButton(text="Список всех лекарств")
+        button_3 = types.KeyboardButton(text="Проверить лекарство")
+        button_4 = types.KeyboardButton(text="Проверить сроки лекарств")
+        button_5 = types.KeyboardButton(text="Список всех лекарств")
         keyboard.row(button_1, button_2)
         keyboard.row(button_3, button_4)
         bot.send_message(message.chat.id, 'Что делаем?', reply_markup=keyboard)
@@ -52,9 +53,11 @@ def del_medicine(message):
         if len(data["medicine"]) == 0:
             bot.send_message(message.chat.id, "Пусто")
         else:
-            for med in  data["medicine"]:
-                button = types.InlineKeyboardButton(text=(' '.join(map(str, [med, data["medicine"][med]["type"], data["medicine"][med]["date"]]))), callback_data="del_"+med)
-                keyboard.row(button)
+            for type in  data["medicine"]:
+                if len(data["medicine"][type]) != 0:
+                    for med in data["medicine"][type]:
+                        button = types.InlineKeyboardButton(text=(' '.join(map(str, [med, type, data["medicine"][type][med]["date"]]))), callback_data="del_"+med+"_"+type)
+                        keyboard.row(button)
             bot.send_message(message.chat.id, 'Какое лекарство удалить?', reply_markup=keyboard)
 
 @bot.message_handler(func=lambda message: message.content_type == 'text' and message.reply_to_message is not None and message.json['reply_to_message']['text'] == 'Какое лекарство?')
@@ -62,12 +65,16 @@ def check_medicine(message):
     if message.chat.id in config.adminid:
         json_data = open("apteka.json", encoding='utf-8').read()
         data = json.loads(json_data)
-        try:
-            if data["medicine"][message.text.lower()] is None:
-                bot.send_message(message.chat.id, "Нет лекарства в аптечке")
-            else:
-                bot.send_message(message.chat.id, "Есть: "+message.text.lower()+" "+ data["medicine"][message.text.lower()]["type"]+" "+data["medicine"][message.text.lower()]["date"] )
-        except KeyError:
+        k=0
+        for type in  data["medicine"]:
+            if len(data["medicine"][type]) != 0:
+                try:
+                    if data["medicine"][type][message.text.lower()]:
+                        bot.send_message(message.chat.id, "Есть: "+message.text.lower()+" "+ type +" "+data["medicine"][type][message.text.lower()]["date"] )
+                        k+=1
+                except KeyError:
+                    pass
+        if k==0:
             bot.send_message(message.chat.id, "Нет лекарства в аптечке")
         apteka(message)
 
@@ -93,57 +100,64 @@ def check_med(message):
         data = json.loads(json_data)
         now=datetime.now()
         k=0
-        for med in data["medicine"]:
-            try:
-                time_med= DT.datetime.strptime(data["medicine"][med]["date"],'%d.%m.%Y')
-            except ValueError:
-                time_med = DT.datetime.strptime(data["medicine"][med]["date"], '%m.%Y')
-            if now > time_med:
-                bot.send_message(message.chat.id, "Срок годности "+str(med)+" истек: "+ str(data["medicine"][med]["date"]))
-                k+=1
+        for type in data["medicine"]:
+            for med in data["medicine"][type]:
+                format_time= ['%d.%m.%y','%d.%m.%Y', '%m.%Y']
+                for fmt in format_time:
+                    try:
+                        time_med= DT.datetime.strptime(data["medicine"][type][med]["date"],fmt)
+                        break
+                    except ValueError:
+                        pass
+                if now > time_med:
+                    bot.send_message(message.chat.id, "Срок годности "+str(med)+" истек: "+ str(data["medicine"][type][med]["date"]))
+                    k+=1
         if k==0:
             bot.send_message(message.chat.id,"Нет просроченных")
         apteka(message)
 
 @bot.message_handler(func=lambda message: message.content_type == 'text' and message.reply_to_message is not None and message.json['reply_to_message']['text'] == 'Добавить лекарство?')
-def check_medicine(message):
+def check_med(message):
     if message.chat.id in config.adminid:
         global name
         name=message.text.lower()
         json_data = open("apteka.json", encoding='utf-8').read()
         global data
         data = json.loads(json_data)
-        try:
-            if data["medicine"][message.text.lower()] is not None:
-                bot.send_message(message.chat.id, "Такое лекарство уже есть...")
-                #добавить возможность добавки второго
-        except KeyError:
-            keyboard0 = types.InlineKeyboardMarkup()
+        keyboard0 = types.InlineKeyboardMarkup()
+        for type in config.list_med:
+            button = types.InlineKeyboardButton(text=type, callback_data=type)
+            keyboard0.row(button)
+        bot.send_message(message.chat.id, 'Тип лекарства?', reply_markup=keyboard0)
+        @bot.callback_query_handler(func=lambda call: call.data in config.list_med)
+        def callback_inline(call):
+            global type_med
+            type_med=call.data
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=type_med)
+            keyboard1 = types.ForceReply(selective=True)
+            bot.send_message(message.chat.id, 'Срок годности лекарства', reply_markup=keyboard1)
+            @bot.message_handler(func=lambda message: message.content_type == 'text' and message.reply_to_message is not None and 'Срок годности лекарства' in message.json['reply_to_message']['text'])
+            def add_date(message):
+                date_line = re.search('\d{2}.\d{2}.\d{4}|\d{2}.\d{2}.\d{2}|\d{2}.\d{4}|\d{2}.\d{2}', message.text)
+                date_l = date_line.group(0)
+                try:
+                    if data["medicine"][type_med][name]:
+                        bot.send_message(message.chat.id, "Такое лекарство уже есть...")
+                        apteka(message)
+                except KeyError:
+                    try:
+                        if data["medicine"][type_med]:
+                            pass
+                    except KeyError:
+                        data["medicine"][type_med] = {}
 
-            for type in config.list_med:
-                button = types.InlineKeyboardButton(text=type, callback_data=type)
-                keyboard0.row(button)
-            bot.send_message(message.chat.id, 'Тип лекарства?', reply_markup=keyboard0)
-
-            @bot.callback_query_handler(func=lambda call: call.data in config.list_med)
-            def callback_inline(call):
-                global type_med
-                type_med=call.data
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=type_med)
-                keyboard1 = types.ForceReply(selective=True)
-                bot.send_message(message.chat.id, 'Срок годности лекарства', reply_markup=keyboard1)
-
-                @bot.message_handler(func=lambda message: message.content_type == 'text' and message.reply_to_message is not None and 'Срок годности лекарства' in message.json['reply_to_message']['text'])
-                def add_date(message):
-                    date_line = re.search('\d{2}.\d{2}.\d{4}|\d{2}.\d{2}.\d{2}|\d{2}.\d{4}|\d{2}.\d{2}', message.text)
-                    date_l = date_line.group(0)
-                    data["medicine"][name] = {"type": type_med, "date": date_l}
+                    data["medicine"][type_med][name] = {"date": date_l}
                     with open('apteka.json', 'w', encoding='utf-8') as f:
-                        str_= json.dumps(data,
-                                         indent=4,
-                                         sort_keys=True,
-                                         separators=(',', ': '),
-                                         ensure_ascii=False)
+                        str_ = json.dumps(data,
+                                          indent=4,
+                                          sort_keys=True,
+                                          separators=(',', ': '),
+                                          ensure_ascii=False)
                         f.write(str(str_))
                         f.close()
                         apteka(message)
@@ -153,9 +167,11 @@ def callback_inline2(call):
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Удалил "+str(call.data[4:]))
     json_data = open("apteka.json", encoding='utf-8').read()
     data = json.loads(json_data)
-
-    del data["medicine"][call.data[4:]]
-
+    med = (re.search(r'^\w*_',call.data[4:])).group(0)[:-1]
+    type_med = (re.search(r'_\w*$',call.data[4:])).group(0)[1:]
+    del data["medicine"][type_med][med]
+    if len(data["medicine"][type_med]) == 0:
+        del data["medicine"][type_med]
     with open('apteka.json', 'w', encoding='utf-8') as f:
         str_ = json.dumps(data,
                           indent=4,
@@ -164,6 +180,7 @@ def callback_inline2(call):
                           ensure_ascii=False)
         f.write(str(str_))
         f.close()
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
